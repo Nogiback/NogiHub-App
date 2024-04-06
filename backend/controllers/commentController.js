0cf58329b8c08d5554031e0df54dac35f506c2a3
Nogiback <peter.toan.do@gmail.com>
2024-04-06 13:25:04 -0400

    adding comment controller

---
+++ backend/controllers/commentController.js
@@ -0,0 +1,32 @@
+import asyncHandler from "express-async-handler";
+import { body, validationResult } from "express-validator";
+import Comment from "../models/Comment.js";
+
+// GET ALL COMMENTS
+
+export const getAllComments = asyncHandler(async (req, res, next) => {
+  const allComments = await Comment.find()
+    .sort({ createdAt: -1 })
+    .populate({ path: "author", select: "displayName username" })
+    .exec();
+
+  if (!allComments) {
+    res.status(404).json({ message: "Error: No comments found." });
+    return;
+  }
+
+  res.status(200).json(allComments);
+});
+
+// GET SINGLE COMMENT
+
+export const getComment = asyncHandler(async (req, res, next) => {
+  const comment = await Comment.findById(req.params.commentID).exec();
+
+  if (!comment) {
+    res.status(404).json({ message: "Error: No comment found." });
+    return;
+  }
+
+  res.status(200).json(comment);
+});
--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,5 +1,13 @@
 import express from "express";
+import verifyUser from "../middlewares/verifyUser.js";
+import {
+  getAllComments,
+  getComment,
+} from "../controllers/commentController.js";
 
 const router = express.Router();
+
+router.get("/", verifyUser, getAllComments);
+router.get("/:commentID", verifyUser, getComment);
 
 export default router;
