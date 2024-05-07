8e011850e23c00cbee45ba77e75f80537bec6412
Peter Do <peter.toan.do@gmail.com>
2024-05-07 19:27:00 -0400

    Starting post comment input section on post page

--- frontend/src/components/PostDetails/CommentInput.tsx
+++ frontend/src/components/PostDetails/CommentInput.tsx
@@ -1,3 +1,21 @@
+import { useAuthContext } from '../../context/AuthContext';
+
 export default function CommentInput() {
-  return <div>CommentInput</div>;
+  const { authUser } = useAuthContext();
+  return (
+    <div className='container flex items-center gap-4 border-b border-base-300 pb-4'>
+      <div className='avatar mt-2 self-start'>
+        <div className='w-12 rounded-full ring ring-primary ring-offset-2 ring-offset-base-100'>
+          <img
+            loading='lazy'
+            src={authUser?.profilePic}
+            alt='user profile picture'
+          />
+        </div>
+      </div>
+      <div className='container flex'>
+        <form>{/* Add comment input here */}</form>
+      </div>
+    </div>
+  );
 }
